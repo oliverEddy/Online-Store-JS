f108ae946d46c5ca80bda983560f31282e0fa516
OLIVER EDDY <oliver.eddy@developersinstitute.co.nz>
2022-09-02 14:58:04 +1200

    Set up envirment for auth0.

--- server/middleware/authorizationMiddleware.js
+++ server/middleware/authorizationMiddleware.js
@@ -1,8 +1,8 @@
 const { auth, requiredScopes } = require("express-oauth2-jwt-bearer");
 
 const checkJwt = auth({
-  audience: "",
-  issuerBaseURL: "",
+  audience: "https://catcouture.co.nz",
+  issuerBaseURL: "https://dev-8sarvpl3.us.auth0.com/",
 });
 
 const checkScopes = requiredScopes("");
