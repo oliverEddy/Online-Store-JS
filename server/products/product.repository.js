2077a9d63f75c9740db665a43c91fe13ebcc641e
olle1134 <99058610+olle1134@users.noreply.github.com>
2022-09-07 16:43:17 +1200

    resolving merge conflcis 2.

--- server/products/product.repository.js
+++ server/products/product.repository.js
@@ -43,6 +43,9 @@ module.exports = {
   },
   getTotalProducts: async (limit, page) => {
     try {
+      if (page <= 0) {
+        throw new Error("page must be greater than 0");
+      }
       const offset = limit * (page - 1);
       const result = await db.query(getPagedProductsSql, [limit, offset]);
       return result.rows;
