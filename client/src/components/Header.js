41d267daa9f5f12fac31fe391f2faaa942206442
olle1134 <99058610+olle1134@users.noreply.github.com>
2022-09-08 17:14:34 +1200

    A few touch ups to my css

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -38,7 +38,7 @@ const Header = () => {
         </nav>
       </header>
       <header className={`${styles.header} main-layout section-padding`}>
-        <h1 className="logo">Cat Couture</h1>
+        <h1 className="logo">Cat Couture 𓃠</h1>
       </header>
     </>
   );
